23f8a79388e39d6b038001fdd66bea35d0de2ef4
lavazure <coolepic238+github@gmail.com>
2025-11-02 12:33:53 +0100

    Update handlers.hpp

--- handlers.hpp
+++ handlers.hpp
@@ -10,86 +10,57 @@
 #include <functional>
 
 namespace brutal {
+
 // forward
 struct entity;
 
 // event handlers
-/*
-    websocket open
-*/
-typedef std::function<void(void)>                        open_handler;
 
-/*
-    websocket close
-*/
-typedef std::function<void(void)>                        close_handler;
+// websocket open
+typedef std::function<void(void)>                       open_handler;
 
-/*
-    websocket error
-*/
-typedef std::function<void(std::error_code&)>            error_handler;
+// websocket close
+typedef std::function<void(void)>                       close_handler;
 
-/*
-    websocket message
-*/
-typedef std::function<void(std::vector<uint8_t>&)>       message_handler;
+// websocket error
+typedef std::function<void(std::error_code&)>           error_handler;
 
-/*
-    received id
-*/
-typedef std::function<void(uint16_t)>                    enter_game_handler;
+// websocket message
+typedef std::function<void(std::vector<uint8_t>&)>      message_handler;
 
-/*
-    received map config
-*/
-typedef std::function<void(uint8_t,float,float)>         map_handler;
+// received id
+typedef std::function<void(uint16_t)>                   enter_game_handler;
 
-/*
-    deleted by someone
-*/
-typedef std::function<void(uint16_t,std::u16string)>     death_handler;
+// received map config
+typedef std::function<void(uint8_t, float, float)>     map_handler;
 
-/*
-    deleted someone
-*/
-typedef std::function<void(uint16_t,std::u16string)>     kill_handler;
+// deleted by someone
+typedef std::function<void(uint16_t, std::u16string)>  death_handler;
 
-/*
-    got king packet
-*/
-typedef std::function<void(uint16_t,float,float)>        king_handler;
+// deleted someone
+typedef std::function<void(uint16_t, std::u16string)>  kill_handler;
 
-/*
-    received leaderboard stats
-*/
-typedef std::function<void(leaderboard&)>                leaderboard_handler;
+// got king packet
+typedef std::function<void(uint16_t, float, float)>    king_handler;
 
-/*
-    pong by server
-*/
-typedef std::function<void(void)>                        pong_handler;
+// received leaderboard stats
+typedef std::function<void(leaderboard&)>              leaderboard_handler;
 
-/*
-    receieved minimap packet
-*/
-typedef std::function<void(minimap&)>                    minimap_handler;
+// pong by server
+typedef std::function<void(void)>                       pong_handler;
 
-/*
-    entity update
-*/
-typedef std::function<void(entity*)>                     entity_update_handler;
+// received minimap packet
+typedef std::function<void(minimap&)>                  minimap_handler;
 
-/*
-    entity create
-*/
-typedef std::function<void(entity*)>                     entity_create_handler;
+// entity update
+typedef std::function<void(entity*)>                   entity_update_handler;
 
-/*
-    entity delete
-*/
-typedef std::function<void(entity*)>                     entity_delete_handler;
+// entity create
+typedef std::function<void(entity*)>                   entity_create_handler;
+
+// entity delete
+typedef std::function<void(entity*)>                   entity_delete_handler;
 
 } // namespace brutal
-
 
 #endif

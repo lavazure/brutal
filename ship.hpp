aa3ae3cc41c2f466069f76c71f28de904ee18ea0
lavazure <coolepic238+github@gmail.com>
2025-11-03 18:43:45 +0530

    Update ship.hpp

--- ship.hpp
+++ ship.hpp
@@ -1,23 +1,21 @@
 #ifndef BRUTAL_SHIP_HPP
 #define BRUTAL_SHIP_HPP
 
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
 #include "entity.hpp"
 #include "opcodes.hpp"
 #include "utils.hpp"
 
-#include <vector>
-#include <cstdint>
-#include <cstddef>
-#include <cstring>
-
-namespace brutal
-{
+namespace brutal {
 
 /*
     A class representing the flail
 */
-struct flail_type
-{
+struct flail_type {
     float x;
     float y;
     float angle;
@@ -27,17 +25,15 @@ struct flail_type
 /*
     A class representing a chain segment
 */
-struct chain_segment
-{
+struct chain_segment {
     float x;
     float y;
 };
 
 /*
-    A class representing the player/ship.
+    A class representing the player
 */
-struct ship : public entity
-{
+struct ship : public entity {
     explicit ship()
         : entity(opcodes::entities::player, 0),
           transfer_energy(0),
@@ -57,9 +53,7 @@ struct ship : public entity
           being_deleted(false),
           red_flail(false),
           red_flail_deployed(false),
-          red_flail_time(0)
-    {
-    }
+          red_flail_time(0) {}
 
     uint16_t transfer_energy;
     uint32_t energy;
@@ -102,13 +96,10 @@ struct ship : public entity
     bool red_flail_deployed;
     uint8_t red_flail_time;
 
-    inline size_t update_chain_flail(std::vector<uint8_t>& data, size_t offset, bool is_full)
-    {
+    inline size_t update_chain_flail(std::vector<uint8_t>& data, size_t offset, bool is_full) {
         uint8_t num_segments = data[offset++];
-        for (size_t i = 0; i < num_segments; ++i)
-        {
-            if (is_full && i >= chain_segments.size())
-            {
+        for (size_t i = 0; i < num_segments; ++i) {
+            if (is_full && i >= chain_segments.size()) {
                 chain_segments.push_back({0.0f, 0.0f});
             }
 
@@ -129,8 +120,7 @@ struct ship : public entity
         return offset;
     }
 
-    inline size_t update_network_flail(std::vector<uint8_t>& data, size_t offset, bool is_full, uint8_t op)
-    {
+    inline size_t update_network_flail(std::vector<uint8_t>& data, size_t offset, bool is_full, uint8_t op) {
         float cur_x;
         float cur_y;
         float cur_angle;
@@ -150,12 +140,10 @@ struct ship : public entity
         flail_radius = utils::energy_to_radius(energy);
 
         uint16_t flags = 0;
-        if (op == 0xB3) // OPCODE_ENTITY_INFO_V2
+        if (op == 0xB3)  // OPCODE_ENTITY_INFO_V2
         {
             flags = data[offset++];
-        }
-        else
-        {
+        } else {
             std::memcpy(&flags, data.data() + offset, sizeof(flags));
             offset += sizeof(flags);
         }
@@ -169,13 +157,11 @@ struct ship : public entity
         inside = flags & flag_inside;
         charging = flags & flag_charging;
 
-        if (op != 0xB3)
-        {
+        if (op != 0xB3) {
             red_flail = flags & flag_red_flail;
             red_flail_deployed = flags & flag_red_flail_deployed;
 
-            if (red_flail_deployed)
-            {
+            if (red_flail_deployed) {
                 red_flail_time = data[offset++];
             }
         }
@@ -188,8 +174,7 @@ struct ship : public entity
         return offset;
     }
 
-    virtual size_t update_network(std::vector<uint8_t>& data, size_t offset, bool is_full, uint8_t op) override
-    {
+    size_t update_network(std::vector<uint8_t>& data, size_t offset, bool is_full, uint8_t op) override {
         float cur_x;
         float cur_y;
         float cur_angle;
@@ -209,23 +194,20 @@ struct ship : public entity
         offset = update_chain_flail(data, offset, is_full);
         offset = update_network_flail(data, offset, is_full, op);
 
-        if (is_full)
-        {
+        if (is_full) {
             std::memcpy(&hue, data.data() + offset, sizeof(hue));
             offset += sizeof(hue);
-            
         }
-        
+
         return offset;
     }
 
-    virtual size_t delete_network(std::vector<uint8_t>& /*data*/, size_t offset) override
-    {
+    size_t delete_network(std::vector<uint8_t>& data, size_t offset) override {
         being_deleted = true;
         return offset;
     }
 };
 
-} // namespace brutal
+}  // namespace brutal
 
-#endif // BRUTAL_SHIP_HPP
+#endif  // BRUTAL_SHIP_HPP

75ea9ca90b079273747e68773dc7f227fefbf170
lavazure <coolepic238+github@gmail.com>
2025-11-03 18:42:28 +0530

    Update red_flail_powerup.hpp

--- red_flail_powerup.hpp
+++ red_flail_powerup.hpp
@@ -1,27 +1,22 @@
 #ifndef BRUTAL_RED_FLAIL_POWERUP_HPP
 #define BRUTAL_RED_FLAIL_POWERUP_HPP
 
-#include "entity.hpp"
-#include "opcodes.hpp"
-
-#include <vector>
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <cstring>
+#include <vector>
 
-namespace brutal
-{
+#include "opcodes.hpp"
+#include "entity.hpp"
 
+namespace brutal {
 /*
     A class representing the red flail powerup
     type: 4
     subtype: 4
 */
-struct red_flail_powerup : public entity
-{
-    red_flail_powerup()
-        : entity(opcodes::entities::item, opcodes::entities::red_flail)
-    {}
+struct red_flail_powerup : public entity {
+    red_flail_powerup() : entity(opcodes::entities::item, opcodes::entities::red_flail) {}
 
     uint16_t energy;
     uint16_t hue;
@@ -29,8 +24,7 @@ struct red_flail_powerup : public entity
     bool being_deleted;
     bool flail_grabbed;
 
-    virtual size_t update_network(std::vector<uint8_t>& data, size_t offset, bool /*is_full*/) override
-    {
+    size_t update_network(std::vector<uint8_t>& data, size_t offset, bool /*is_full*/) override {
         float cur_x;
         float cur_y;
 
@@ -46,16 +40,15 @@ struct red_flail_powerup : public entity
         return offset;
     }
 
-    virtual size_t delete_network(std::vector<uint8_t>& data, size_t offset) override
-    {
+    size_t delete_network(std::vector<uint8_t>& data, size_t offset) override {
         flail_grabbed = static_cast<bool>(data[offset]);
         offset += 1;
-        
         being_deleted = true;
+        
         return offset;
     }
 };
 
-} // namespace brutal
+}  // namespace brutal
 
-#endif // BRUTAL_RED_FLAIL_POWERUP_HPP
+#endif  // BRUTAL_RED_FLAIL_POWERUP_HPP

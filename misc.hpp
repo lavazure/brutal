a1cc3132e3902a264902a7b113ddfbfe521cc4be
lavazure <coolepic238+github@gmail.com>
2025-11-04 17:37:56 +0530

    Update misc.hpp

--- misc.hpp
+++ misc.hpp
@@ -2,8 +2,32 @@
 #define BRUTAL_MISC_HPP
 
 #include <cstdint>
+#include <unordered_map>
+
+#include "entity.hpp"
+
+#define get_typed(entity) \
+    if ((entity)->type == 5) \
+        return *reinterpret_cast<brutal::ship*>(entity); \
+    else if ((entity)->type == 1) \
+        return *reinterpret_cast<brutal::collider*>(entity); \
+    else if ((entity)->type == 4 && (entity)->subtype == 0) \
+        return *reinterpret_cast<brutal::atom*>(entity); \
+    else if ((entity)->type == 4 && (entity)->subtype == 1) \
+        return *reinterpret_cast<brutal::energy*>(entity); \
+    else if ((entity)->type == 4 && ((entity)->subtype == 2 || (entity)->subtype == 3)) \
+        return *reinterpret_cast<brutal::tri*>(entity); \
+    else if ((entity)->type == 4 && (entity)->subtype == 4) \
+        return *reinterpret_cast<brutal::red_flail_powerup*>(entity);
+
 
 namespace brutal {
+// Entity Pointer
+typedef entity* entity_ptr;
+
+// Entity Compnent System
+class entity_map {};
+
 // leaderboard
 struct leaderboard_item {
     std::u16string nick;

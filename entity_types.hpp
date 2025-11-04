d62021b5050809f5f6bb205c30d26f45bbb8c9c4
lunarfractal <xxs239239@gmail.com>
2025-11-04 16:02:31 +0100

    new

--- entity_types.hpp
+++ entity_types.hpp
@@ -4,11 +4,30 @@
 #include <memory>
 
 #include "entity.hpp"
+#include "atom.hpp"
+#include "ship.hpp"
+#include "collider.hpp"
+#include "energy.hpp"
+#include "tri.hpp"
+#include "red_flail_powerup.hpp"
 
 namespace brutal {
-// Typedef Entity Pointer
+// Typedef Entity Pointers
 typedef std::shared_ptr<entity> entity_ptr;
 
+typedef std::shared_ptr<atom> atom_ptr;
+
+typedef std::shared_ptr<energy> energy_ptr;
+
+typedef std::shared_ptr<collider> collider_ptr;
+
+typedef std::shared_ptr<tri> tri_ptr;
+
+typedef std::shared_ptr<ship> ship_ptr;
+
+typedef std::shared_ptr<red_flail_powerup> red_flail_ptr;
+
+typedef std::shared_ptr<red_flail_powerup> redflail_ptr;
 }
 
 #endif // BRUTAL_ENTITY_TYPES_HPP
--- misc.hpp
+++ misc.hpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 
 #include "entity_types.hpp"
+#include "opcodes.hpp"
 
 #define get_typed(entity) \
     if ((entity)->type == 5) \
@@ -26,15 +27,85 @@ namespace brutal {
 class entity_map {
 public:
     entity_ptr& operator[](uint16_t id) {
-        auto it = m_map.find(id);
-        if(it != m_map.end()) return it->second;
-        else return nullptr;
+        return m_map[id];
+    }
+
+    std::unordered_map<uint16_t, ship_ptr> get_ship_map() {
+        std::unordered_map<uint16_t, ship_ptr> ship_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::ship) return;
+            ship_map[pair.first] = pair.second;
+        }
+
+       return ship_map;
+    }
+
+    std::unordered_map<uint16_t, atom_ptr> get_atom_map() {
+        std::unordered_map<uint16_t, atom_ptr> atom_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::item || pair.second->subtype != opcodes::entities::atom) return;
+            atom_map[pair.first] = pair.second;
+        }
+
+       return atom_map;
+    }
+
+    std::unordered_map<uint16_t, energy_ptr> get_energy_map() {
+        std::unordered_map<uint16_t, energy_ptr> energy_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::item || pair.second->subtype != opcodes::entities::energy) return;
+            energy_map[pair.first] = pair.second;
+        }
+
+       return energy_map;
+    }
+
+    std::unordered_map<uint16_t, collider_ptr> get_collider_map() {
+        std::unordered_map<uint16_t, collider_ptr> collider_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::collider) return;
+            collider_map[pair.first] = pair.second;
+        }
+
+       return collider_map;
+    }
+
+    std::unordered_map<uint16_t, tri_ptr> get_tri_map() {
+        std::unordered_map<uint16_t, tri_ptr> tri_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::item || (pair.second->subtype != opcodes::entities::tri_plus && pair.second->subtype != opcodes::entities::tri_minus)) return;
+            tri_map[pair.first] = pair.second;
+        }
+
+       return tri_map;
+    }
+
+    std::unordered_map<uint16_t, redflail_ptr> get_redflail_map() {
+        std::unordered_map<uint16_t, redflail_ptr> redflail_map;
+
+        for (auto& pair: m_map) {
+            if(pair.second->type != opcodes::entites::item || pair.second->subtype != opcodes::entities::redflail) return;
+            redflail_map[pair.first] = pair.second;
+        }
+
+       return redflail_map;
+    }
+
+    std::unordered_map<uint16_t, redflail_ptr> get_red_flail_map() {
+        return get_redflail_map();
     }
 
     void erase(uint16_t id) {
         auto it = m_map.find(id);
         if(it != m_map.end()) m_map.erase(it);
     }
+
+    std
 
 private:
     std::unordered_map<uint16_t, entity_ptr> m_map;
